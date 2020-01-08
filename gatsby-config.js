b5abfb0daf184310b816ace448da95b55dfabeb6
gshah2020 <gaurang.r.shah@gmail.com>
2020-01-07 21:19:11 -0500

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,51 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
+module.exports = {
+  siteMetadata: {
+    title: `Jammers Style Guide Starter`,
+    description: `Now you're jammin with us.`,
+    author: `@gatsbyjs`
+  },
+  plugins: [
+    `gatsby-mdx`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        precision: 6
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GA_TRACKING_ID
+      }
+    }
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ]
+};
